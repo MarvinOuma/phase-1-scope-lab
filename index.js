ed9ef681f8a125a8fa4e2cd80ff4040039e16405
MarvinDanielOduor <marvin.ouma@strathmore.edu>
2025-03-11 12:13:45 +0300

    Completed Scope Lab (CodeGrade)

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Write your solution in this file!
+// index.js
+
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+  return customerName;
+}
+
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'some customer';
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'another customer';
+}
+
+if (typeof module !== 'undefined' && module.exports) {
+  module.exports = {
+    customerName,
+    upperCaseCustomerName,
+    setBestCustomer,
+    overwriteBestCustomer,
+    leastFavoriteCustomer,
+    changeLeastFavoriteCustomer,
+  };
+}
